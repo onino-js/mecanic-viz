d1089c976cc6905a67993497f5e4e117dd9dd1ba
onino <seba.pinard@gmail.com>
2025-06-16 11:57:59 +0200

    delete table page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,7 @@
 import { Routes } from '@angular/router';
 import { ChartsComponent } from './components/charts/charts.component';
-import { TablesComponent } from './components/tables/tables.component';
 
 export const routes: Routes = [
   { path: 'charts', component: ChartsComponent },
-  { path: 'tables', component: TablesComponent },
   { path: '', redirectTo: '/charts', pathMatch: 'full' },
 ];
